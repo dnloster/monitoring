ad8790aee9db0750d48c7c4dd09c5b95888b24da
dnloster <ducgt.ng@gmail.com>
2024-05-13 14:10:40 +0700

    first commit

---
+++ src/components/FidelisSystem.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+
+import DataTable from "datatables.net-vue3";
+import DataTablesCore from "datatables.net-bs5";
+
+DataTable.use(DataTablesCore);
+
+const columns = [
+    { data: "name" },
+    { data: "ip_address" },
+    { data: "status" },
+];
+
+</script>
+
+<template>
+    <div class="shadow-box mb-3" :style="boxStyle">
+        <div class="list-header">
+            <div class="header-title">
+                <h1 class="text-center mt-3 mb-5">Fidelis System Health Data</h1>
+            </div>
+        </div>
+        <DataTable :columns="columns" ajax="/data_fidelis.json" class="table table-hover" width="100%">
+            <thead class="border-top-0">
+                <tr class="text-white">
+                    <th>Tên máy chủ</th>
+                    <th>Địa chỉ IP</th>
+                    <th>Trạng thái</th>
+                </tr>
+            </thead>
+        </DataTable>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import 'bootstrap';
+@import 'datatables.net-bs5';
+@import "../assets/vars.scss";
+
+.list-header {
+    border-bottom: 1px solid #dee2e6;
+    border-radius: 10px 10px 0 0;
+    margin: -10px;
+    margin-bottom: 10px;
+    padding: 10px;
+
+    .dark & {
+        background-color: $dark-header-bg;
+        border-bottom: 0;
+    }
+}
+
+.header-top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+@media (max-width: 770px) {
+    .list-header {
+        margin: -20px;
+        margin-bottom: 10px;
+        padding: 5px;
+    }
+}
+
+.table > tbody > tr:nth-of-type(odd) {
+    background-color: #070a10 !important;
+    color: white !important;
+}
+</style>
---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div v-if="!$root.isMobile" class="col-12 col-md-5 col-xl-6" style="max-height: calc((100vh - 76px) / 2); overflow: scroll">
+                <div>
+                    <router-link to="/add" class="btn btn-primary mb-3"><font-awesome-icon icon="plus" /> {{ $t("Add New Monitor") }}</router-link>
+                </div>
+                <MonitorList :scrollbar="true" />
+            </div>
+
+            <div ref="container" class="col-12 col-md-7 col-xl-6 mb-3" style="max-height: calc((100vh - 76px) / 2); overflow: scroll">
+                <!-- Add :key to disable vue router re-use the same component -->
+                <router-view :key="$route.fullPath" :calculatedHeight="height" />
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-6 col-xl-6">
+                <QradarSystem :scrollbar="true" />
+            </div>
+            <div class="col-12 col-md-6 col-xl-6">
+                <FidelisSystem :scrollbar="true" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import MonitorList from "../components/MonitorList.vue";
+import QradarSystem from "../components/QradarSystem.vue";
+import FidelisSystem from "../components/FidelisSystem.vue";
+
+export default {
+    components: {
+        MonitorList,
+        QradarSystem,
+        FidelisSystem,
+    },
+    data() {
+        return {
+            height: 0
+        };
+    },
+    mounted() {
+        this.height = this.$refs.container.offsetHeight;
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.container-fluid {
+    width: 98%;
+}
+</style>
